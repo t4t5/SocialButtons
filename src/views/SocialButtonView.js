3c962b1150827301e06d99ba4f6e5d837a9616b6
marcker <marckfree@gmail.com>
2014-10-16 11:11:37 -0300

    Fixes file name and prevents the occurrence of repeats in the animations
    * Fixes file name that was causing 'not found' error: 'socialButtonView.js' to 'SocialButtonView.js'
    * Prevent the occurrence of repetitions in the animations when mouse events are activated repeatedly.

--- src/views/SocialButtonView.js
+++ src/views/SocialButtonView.js
@@ -12,16 +12,16 @@ define(function(require, exports, module) {
 
     function SocialButtonView() {
       View.apply(this, arguments);
-      
+
       _createButtonBg.call(this);
       _createButtonCircleBorder.call(this);
       _createButtonIcon.call(this);
       _createButtonContainer.call(this);
-      
+
       _addHoverEffect.call(this);
       _addClickableLink.call(this);
     }
-    
+
     SocialButtonView.prototype = Object.create(View.prototype);
     SocialButtonView.prototype.constructor = SocialButtonView;
 
@@ -31,113 +31,117 @@ define(function(require, exports, module) {
       backgroundColor: 'gray',
       iconUrl: 'img/twitter-icon.svg',
     };
-    
-    
-    
+
+
+
     function _createButtonContainer() {
       this.buttonContainer = new Surface({
-  	    size: [this.options.buttonSize, this.options.buttonSize],
-  	    properties: {
-    	    cursor: 'pointer'
-  	    }
+          size: [this.options.buttonSize, this.options.buttonSize],
+          properties: {
+            cursor: 'pointer'
+          }
       });
       this.buttonModifier = new StateModifier({
         transform: Transform.translate(0, 0, 0.1)
       });
-      
+
       this.add(this.buttonModifier).add(this.buttonContainer);
     }
-    
-    
+
+
     function _createButtonCircleBorder() {
       this.buttonCircle = new Surface({
-  	    size: [this.options.buttonSize, this.options.buttonSize],
-  	    properties: {
-    	    border: '1px solid '+this.options.borderColor,
-    	    borderRadius: '50%'
-  	    }
+          size: [this.options.buttonSize, this.options.buttonSize],
+          properties: {
+            border: '1px solid '+this.options.borderColor,
+            borderRadius: '50%'
+          }
       });
       this.circleModifier = new StateModifier();
-      
+
       this.add(this.circleModifier).add(this.buttonCircle);
     }
-      
-      
+
+
     function _createButtonBg() {
       this.buttonBg = new Surface({
-  	    size: [this.options.buttonSize, this.options.buttonSize],
-  	    opacity: 0,
-  	    properties: {
+          size: [this.options.buttonSize, this.options.buttonSize],
+          opacity: 0,
+          properties: {
           backgroundColor: this.options.backgroundColor,
-    	    borderRadius: '50%'
-  	    }
+            borderRadius: '50%'
+          }
       });
       this.buttonBgModifier = new StateModifier({
         transform: Transform.scale(0, 0, 0.1)
       });
-      
+
       this.add(this.buttonBgModifier).add(this.buttonBg);
     }
-    
+
     function _createButtonIcon() {
       this.socialIcon = new Surface({
         size: [this.options.buttonSize/2, this.options.buttonSize/2],
         properties: {
           backgroundImage: 'url('+this.options.iconUrl+')',
-    	    backgroundSize: this.options.buttonSize/2+'px',
-    	    backgroundPosition: 'center center',
-    	    backgroundRepeat: 'no-repeat'
+            backgroundSize: this.options.buttonSize/2+'px',
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat'
         }
       });
-     
+
       this.add(this.socialIcon);
     }
-  
-    
-    
+
+
+
     // Hover effect
-    
+
     function _addHoverEffect() {
-    
+
       this.buttonContainer.on('mouseover', function(){
+        this.circleModifier.halt();
         this.circleModifier.setTransform(
           Transform.scale(1.3, 1.3, 1), { duration: 200, curve: 'easeInOut' }
         ).setOpacity(
           0, { duration: 300, curve: 'easeInOut' }
         );
-        
+
+        this.buttonBgModifier.halt();
         this.buttonBgModifier.setTransform(   // Set inital size (tiny)...
           Transform.scale(0.1, 0.1, 1)
         ).setTransform(                       // ... then scale it up!
-          Transform.scale(1, 1, 1), 
+          Transform.scale(1, 1, 1),
           { method: 'spring', period: 300, dampingRatio: 0.5 }
         ).setOpacity(
           1, { duration: 300, curve: 'easeInOut' }
-        );  
+        );
       }.bind(this));
-      
-      
+
+
       this.buttonContainer.on('mouseout', function(){
+        this.circleModifier.halt();
         this.circleModifier.setTransform(
           Transform.scale(1, 1, 1)
         ).setOpacity(
           1, { duration: 200 }
         );
-    
+
+        this.buttonBgModifier.halt();
         this.buttonBgModifier.setOpacity(
           0, { duration: 300, curve: 'easeInOut' }
-        );  
+        );
       }.bind(this));
 
     }
-    
-    
+
+
     function _addClickableLink() {
       this.buttonContainer.on('click', function(){
         window.open(this.options.url)
       }.bind(this));
     }
-  
+
 
 
     module.exports = SocialButtonView;
